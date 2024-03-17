969019ba75ebd5551576097c69f49bedd4486037
Maxim Cherevko <makscerevko1@gmail.com>
2024-03-18 00:31:36 +0300

    developed operatores =, ==

--- projects/scl/src/long_number/long_number.cpp
+++ projects/scl/src/long_number/long_number.cpp
@@ -1,4 +1,4 @@
-#include "long_number.hpp"
+#include "long_number.h"
 
 namespace MCherevko {
 	LongNumber::LongNumber() {
@@ -7,9 +7,9 @@ namespace MCherevko {
 		length = 1;
 		sign = NULL;
 	}
-	
+
 	LongNumber::LongNumber(const char* const str) {
-		length = get_length(str);
+		length = get_lenght(str);
 
 		if (str[0] == MINUS) {
 			sign = NEGATIVE;
@@ -27,17 +27,17 @@ namespace MCherevko {
 			}
 		}
 	}
-	
+
 	LongNumber::LongNumber(const LongNumber& x) {
 		length = x.length;
 		sign = x.sign;
 		numbers = new int[length];
-		
+
 		for (int i = 0; i < length; ++i) {
 			numbers[i] = x.numbers[i];
 		}
 	}
-	
+
 	LongNumber::LongNumber(LongNumber&& x) {
 		numbers = x.numbers;
 		length = x.length;
@@ -48,97 +48,145 @@ namespace MCherevko {
 		x.length = 1;
 		x.sign = NULL;
 	}
-	
+
 	LongNumber::~LongNumber() {
 		delete[] numbers;
 	}
-	
+
 	LongNumber& LongNumber::operator = (const char* const str) {
-		// TODO
+		delete[] numbers;
+		length = get_lenght(str);
+
+		if (str[0] == MINUS) {
+			sign = NEGATIVE;
+			length--;
+			numbers = new int[length];
+			for (int i = 0; i < length; i++) {
+				numbers[i] = str[i + 1] - STR_NULL;
+			}
+		}
+		else {
+			int sign = POSITIVE;
+			numbers = new int[length];
+			for (int i = 0; i < length; i++) {
+				numbers[i] = str[i] - STR_NULL;
+			}
+		}
 		return *this;
 	}
-	
+
 	LongNumber& LongNumber::operator = (const LongNumber& x) {
-		// TODO
+		numbers = new int[x.length];
+		length = x.length;
+		sign = x.sign;
+
+		for (int i = 0; i < this->length; ++i) {
+			numbers[i] = x.numbers[i];
+		}
 		return *this;
 	}
-	
+
 	LongNumber& LongNumber::operator = (LongNumber&& x) {
-		// TODO
+		if (this != &x) {
+			numbers = x.numbers;
+			length = x.length;
+			sign = x.sign;
+
+			x.numbers = new int[1];
+			x.numbers[0] = 0;
+			x.length = 1;
+			x.sign = NULL;
+		}
+
 		return *this;
 	}
-	
+
 	bool LongNumber::operator == (const LongNumber& x) {
-		// TODO
+		if (length != x.length || sign != x.sign) {
+			return false;
+		}
+		for (int i = 0; i < length; ++i) {
+			if (numbers[i] != x.numbers[i]) {
+				return false;
+			}
+		}
+
 		return true;
 	}
-	
+
 	bool LongNumber::operator > (const LongNumber& x) {
-		// TODO
+
 		return true;
 	}
-	
+
 	bool LongNumber::operator < (const LongNumber& x) {
 		// TODO
 		return true;
 	}
-	
+
 	LongNumber LongNumber::operator + (const LongNumber& x) {
 		// TODO
 		LongNumber result;
 		return result;
 	}
-	
+
 	LongNumber LongNumber::operator - (const LongNumber& x) {
 		// TODO
 		LongNumber result = x;
 		return result;
 	}
-	
+
 	LongNumber LongNumber::operator * (const LongNumber& x) {
 		// TODO
 		LongNumber result;
 		return result;
 	}
-	
+
 	LongNumber LongNumber::operator / (const LongNumber& x) {
 		// TODO
 		LongNumber result;
 		return result;
 	}
-	
+
 	LongNumber LongNumber::operator % (const LongNumber& x) {
 		// TODO
 		LongNumber result;
 		return result;
 	}
-	
+
+	void LongNumber::print_numbers() {
+		for (int i = 0; i < this->length; i++) {
+			std::cout << this->numbers[i] << ' ';
+		}
+		std::cout << std::endl;
+	}
 	int LongNumber::get_digits_number() const {
 		return length;
 	}
-	
+
 	bool LongNumber::is_positive() const {
 		return sign == POSITIVE;
 	}
-	
-	// ----------------------------------------------------------
-	// PRIVATE
-	// ----------------------------------------------------------
-	int LongNumber::get_length(const char* const str) const {
+
+	int LongNumber::get_lenght(const char* const str) const {
 		const char* l = str;
 		while (*l) {
 			++l;
 		}
 		auto length = (l - str);
-		
+
 		return length;
 	}
-	
-	// ----------------------------------------------------------
-	// FRIENDLY
-	// ----------------------------------------------------------
-	std::ostream& operator << (std::ostream &os, const LongNumber& x) {
-		// TODO
+
+	std::ostream& operator << (std::ostream& os, const LongNumber& x) {
+		if (x.sign == -1)
+			os << '-';
+
+		for (int i = 0; i < x.length; ++i) {
+			os << x.numbers[i];
+		}
+
 		return os;
 	}
+
 }
